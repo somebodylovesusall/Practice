17633bdaf4b2e5ba6d46df623185534794d63a14
somebodylovesusall <alleyji126@gmail.com>
2023-06-08 18:34:35 +0900

    readme 수정

--- Algo&DS/arrayChunk.js
+++ Algo&DS/arrayChunk.js
@@ -1,3 +1,14 @@
 /* Array Chunk
-  
+  Given an array and chunk size, divide the array into many subarrays where each subarray is of the provided size.
+
+  Examples:
+  Chunk size 2: chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+  Chunk size 2: chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+  Chunk size 3: chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+  Chunk size 4: chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+  Chunk size 10: chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 */
+
+function chunk(array, size) {
+  
+}
