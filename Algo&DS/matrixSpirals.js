e2e931ddc65ee3bec80471fb3adecd9677f8b273
somebodylovesusall <alleyji126@gmail.com>
2023-06-13 23:12:32 +0900

    matrixSprial

--- Algo&DS/matrixSpirals.js
+++ Algo&DS/matrixSpirals.js
@@ -3,8 +3,8 @@
 
   Examples:
   matrix(2);
-  [[undefined, undefined],
-  [undefined, undefined]]
+  [[1, 2],
+  [4, 3]]
   matrix(3);
   [[1, 2, 3],
   [8, 9, 4],
@@ -15,3 +15,49 @@
   [11, 16, 15, 6],
   [10,  9,  8, 7]]
 */
+
+function matrix(n) {
+  const matrixArr = [];
+
+  for (let i = 0; i < n; i++) {
+    matrixArr.push([]);
+  }
+
+  let count = 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i++) {
+      matrixArr[startRow][i] = count;
+      count = count + 1;
+    }
+  
+    startRow = startRow + 1;
+  
+    for (let i = startRow; i <= endRow; i++) {
+      matrixArr[i][endCol] = count;
+      count = count + 1;
+    }
+  
+    endCol = endCol - 1;
+  
+    for (let i = endCol; i>= startCol; i--) {
+      matrixArr[endRow][i] = count;
+      count = count + 1;
+    }
+  
+    endRow = endRow - 1;
+
+    for (let i = endRow; i >= startRow; i--) {
+      matrixArr[i][startCol] = count;
+      count = count + 1;
+    }
+
+    startCol = startCol + 1;
+  }
+
+  return matrixArr;
+}
