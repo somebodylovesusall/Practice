4e375a77a73a916a7ab68b936207d6bfce2289dd
somebodylovesusall <alleyji126@gmail.com>
2023-06-14 23:43:37 +0900

    fibonacci

--- Algo&DS/fibonacci.js
+++ Algo&DS/fibonacci.js
@@ -8,3 +8,60 @@
   fib(7) === 13;
 */
 
+// 1. iteration: while loop
+function fib1(n) {
+  let num1 = 0;
+  let num2 = 1;
+  let sum = 0;
+  let count = 1;
+
+  while (count < n) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+
+    count = count + 1;
+  }
+
+  return sum;
+}
+
+// 2. iteration: for loop
+function fib2(n) {
+  const fibArr = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+  }
+
+  return fibArr[n];
+}
+
+// 3. recursion
+function fib3(n) {
+  if (n < 2) return n;
+
+  return fib3(n - 2) + fib3(n - 1);
+}
+
+// 4. memoization
+function fib(n) {
+  if (n < 2) return n;
+
+  return fib(n - 2) + fib(n - 1);
+}
+
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) return cache[args];
+  
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+fib = memoize(fib);
