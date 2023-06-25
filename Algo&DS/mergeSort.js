5f315030af9483919a72dc6a46d8a38f65a3e64a
somebodylovesusall <alleyji126@gmail.com>
2023-06-25 10:32:57 +0900

    mergeSort

--- Algo&DS/mergeSort.js
+++ Algo&DS/mergeSort.js
@@ -6,9 +6,22 @@
 */
 
 function mergeSort(arr) {
+  if (arr.length === 1) return arr;
 
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const result = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) result.push(left.shift());
+    else result.push(right.shift());
+  }
+
+  return [...result, ...left, ...right];
 }
